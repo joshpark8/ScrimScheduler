2e561bfb4331cac18c399bddb813e37b1cdd5745
Joshua Park <wljoshpark@gmail.com>
2021-11-26 22:44:00 -0500

    add functionality to count reactions and ping after 6

--- ScrimScheduler.py
+++ ScrimScheduler.py
@@ -5,8 +5,16 @@ from pathlib import Path
 
 from discord.ext import commands
 
+reactionCount = 0
 bot = commands.Bot(command_prefix = '!')
 bot.remove_command('help')
+
+def checkReactions(reactionCount):
+    if reactionCount >= 6:
+        return(True)
+    else:
+        return(False)
+
 
 @bot.event
 async def on_ready():
@@ -14,17 +22,21 @@ async def on_ready():
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    print(f"---------------- TESTING ---------------- \n REACTION: {reaction}\n USER: {user}\n ----------------------------------------")
+    park = await bot.fetch_user("397179931223785484")
+    global reactionCount 
+    reactionCount += 1
     channel = reaction.message.channel
-    await channel.send(f'{user.mention} reacted with {reaction}')
+    if checkReactions(reactionCount):
+        await channel.send(f'{park.mention} You have 6 reactions! Time to book a scrim :)')
 
 @bot.event
 async def on_raw_reaction_remove(payload):
-    print(f"---------------- TESTING ---------------- \n PAYLOAD: {payload}\n ----------------------------------------")
-    print(payload.emoji)
-    channel = await bot.fetch_channel(payload.channel_id)
-    user = await bot.fetch_user(payload.user_id)
-    await channel.send(f'{user.mention} unreacted with {payload.emoji}')
+    global park
+    global reactionCount
+    reactionCount -= 1
+    # channel = await bot.fetch_channel(payload.channel_id)
+    # user = await bot.fetch_user(payload.user_id)
+    # await channel.send(f'{user.mention} unreacted with {payload.emoji}')
 
 @bot.command()
 async def test(context):
