1f9eefdcbcc2377482a8384ef83e6d9efebd7a45
Joshua Park <wljoshpark@gmail.com>
2021-11-26 22:18:06 -0500

    added functionality to detect removal of reactions

--- ScrimScheduler.py
+++ ScrimScheduler.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = '~')
+bot = commands.Bot(command_prefix = '!')
 bot.remove_command('help')
 
 @bot.event
@@ -14,11 +14,17 @@ async def on_ready():
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    
     print(f"---------------- TESTING ---------------- \n REACTION: {reaction}\n USER: {user}\n ----------------------------------------")
-    # emoji = str(reaction)
     channel = reaction.message.channel
     await channel.send(f'{user.mention} reacted with {reaction}')
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    print(f"---------------- TESTING ---------------- \n PAYLOAD: {payload}\n ----------------------------------------")
+    print(payload.emoji)
+    channel = await bot.fetch_channel(payload.channel_id)
+    user = await bot.fetch_user(payload.user_id)
+    await channel.send(f'{user.mention} unreacted with {payload.emoji}')
 
 @bot.command()
 async def test(context):
