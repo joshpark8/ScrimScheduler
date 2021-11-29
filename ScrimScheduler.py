fd87f47a8006fae4e0362e98cd98f8d32cdbe2cd
Joshua Park <wljoshpark@gmail.com>
2021-11-29 15:49:17 -0500

    delete all functions revolving around reactionCount

--- ScrimScheduler.py
+++ ScrimScheduler.py
@@ -5,15 +5,8 @@ from pathlib import Path
 
 from discord.ext import commands
 
-reactionCount = 0
 bot = commands.Bot(command_prefix = '!')
 bot.remove_command('help')
-
-def checkReactions(reactionCount):
-    if reactionCount >= 6:
-        return(True)
-    else:
-        return(False)
 
 @bot.event
 async def on_ready():
@@ -25,27 +18,8 @@ async def scrim(context, role):
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    # global reactionCount 
-    # reactionCount += 1
-    # print(reactionCount)
     channel = reaction.message.channel
     if len(reaction.message.reactions) == 6:
         await channel.send("6 reactions")
-
-# @bot.event
-# async def on_raw_reaction_remove(payload):
-#     global reactionCount
-#     reactionCount -= 1
-#     if reactionCount < 0:
-#         reactionCount = 0
-    # print(reactionCount)
-
-    # channel = await bot.fetch_channel(payload.channel_id)
-    # user = await bot.fetch_user(payload.user_id)
-    # await channel.send(f'{user.mention} unreacted with {payload.emoji}')
-
-# @bot.command()
-# async def test(context):
-#     await context.send("test")
 
 bot.run(os.getenv('ScrimBotKey'))
