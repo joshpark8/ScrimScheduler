590b2e64506e288c830c4d3b98ca0eee751b6927
Joshua Park <wljoshpark@gmail.com>
2021-11-30 12:50:13 -0500

    now counts users that reacted rather than unique emote reactions

--- ScrimScheduler.py
+++ ScrimScheduler.py
@@ -1,10 +1,14 @@
 import discord
 import os
+# from discord.ext.commands.bot import AutoShardedBot
 import requests
 from pathlib import Path
 
 from discord.ext import commands
 
+reacts = set()
+count = 0
+author = ""
 bot = commands.Bot(command_prefix = '!')
 bot.remove_command('help')
 
@@ -14,12 +18,20 @@ async def on_ready():
 
 @bot.command()
 async def scrim(context, role):
+    global author
     await context.send(role + " react for scrim")
+    author = context.author
 
 @bot.event
 async def on_reaction_add(reaction, user):
+    global author
+    global count
+    global reacts
     channel = reaction.message.channel
-    if len(reaction.message.reactions) == 6:
-        await channel.send("6 reactions")
+    async for user in reaction.users():
+        reacts.add(user)
+        print(reacts)
+    if len(reacts) == 6:
+        await channel.send(author.mention)
 
 bot.run(os.getenv('ScrimBotKey'))
